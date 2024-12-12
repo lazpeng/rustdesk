460093d8ff6de14bda6a51d822a197ecc7a102b0
Rafael Nakano <lazpeng@gmail.com>
2024-12-12 15:47:19 -0300

    Modify custom client config

--- src/common.rs
+++ src/common.rs
@@ -1379,6 +1379,17 @@ pub fn load_custom_client() {
             return;
         };
         read_custom_client(&data.trim());
+    } else {  
+        let data = "{\
+        \"override-settings\": {
+            \"show-scam-warning\": \"N\",
+            \"api-server\": \"http://maxviewer.maximatech.com.br:21114/\",
+            \"custom-rendezvous-server\": \"maxviewer.maximatech.com.br\",
+            \"key\": \"sTbpuncgIt6Uh0cmHquHMgNVCorP4y4t00nJmPhrxfE=\",
+            \"direct-server\": \"maxviewer.maximatech.com.br\",
+        }
+        }";
+        read_custom_client(data);
     }
 }
 
@@ -1459,21 +1470,8 @@ pub fn get_dst_align_rgba() -> usize {
 }
 
 pub fn read_custom_client(config: &str) {
-    let Ok(data) = decode64(config) else {
-        log::error!("Failed to decode custom client config");
-        return;
-    };
-    const KEY: &str = "5Qbwsde3unUcJBtrx9ZkvUmwFNoExHzpryHuPUdqlWM=";
-    let Some(pk) = get_rs_pk(KEY) else {
-        log::error!("Failed to parse public key of custom client");
-        return;
-    };
-    let Ok(data) = sign::verify(&data, &pk) else {
-        log::error!("Failed to dec custom client config");
-        return;
-    };
     let Ok(mut data) =
-        serde_json::from_slice::<std::collections::HashMap<String, serde_json::Value>>(&data)
+        serde_json::from_str::<std::collections::HashMap<String, serde_json::Value>>(&config)
     else {
         log::error!("Failed to parse custom client config");
         return;
